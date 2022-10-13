af5f54d27fa1ffc1d735fe963daf47f1b7446de7
isasari <58876142+isasari@users.noreply.github.com>
2022-10-14 01:41:29 +0300

    Add files via upload

---
+++ src/baklava_for.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+
+        System.out.print("Sayı girin :");
+        n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= (n - i); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >0; i--) {
+            for (int k = 1; k <= (n-i) ; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2*i) -1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println("");
+
+        }
+
+
+
+
+    }
+}
