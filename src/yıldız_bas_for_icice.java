02420dd4e0e7da06fdac771a588e6f2ad5f39c04
isasari <58876142+isasari@users.noreply.github.com>
2022-10-14 00:06:57 +0300

    Add files via upload

---
+++ src/yıldız_bas_for_icice.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int n = input.nextInt();
+        int k=1;
+        System.out.println(k);
+
+        for (int i=1; i<=n; i++ ){
+            for (k=1;k<=(n-i);k++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=(2*i-1);j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
