52ac7515cea29bf1919a10707a1fc2d9f18897ac
isasari <58876142+isasari@users.noreply.github.com>
2022-10-25 01:42:20 +0300

    Add files via upload

---
+++ src/bHarfi_arrays.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String list[][] = new String[5][3];
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list[i].length; j++){
+                if ((i == 0 || i == 2 || i == 4)) {
+                    list[i][j] = " * ";
+                } else if (j==0||j==2) {
+                    list[i][j]= " * ";
+                }else list[i][j]= "    ";
+                    
+                }
+            }
+
+        for (String[] row : list) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
